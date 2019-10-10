71f50a40d7dd0bd35b82517453d8757847c7a5ec
Thatresolvestcg <Thatresolvestcg@gmail.com>
2019-10-10 17:22:42 +0100

    managed to get the validation working but I wanted to do it on one line and check all values, struggled and settled for individual checks

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { type } from "os";
+
 // create a function -- this is todo
 // this function will need three arguments
 // we will need to return a string with a currency sign £ so maybe "£" + var
@@ -18,7 +20,11 @@
 
 const calculateBill = (amount, vat, tip) => {
   if (typeof amount !== "number") {
-    console.log("all inputs must be numbers");
+    console.log("all inputs must be numbers, please check the amount value");
+  } else if (typeof vat !== "number") {
+    console.log("all inputs must be numbers, please check the vat value");
+  } else if (typeof tip !== "number") {
+    console.log("all inputs must be numbers, please check the tip value");
   } else {
     incVat = amount + amount * (vat / 100);
     total = incVat + tip;
@@ -26,4 +32,10 @@ const calculateBill = (amount, vat, tip) => {
   }
 };
 
-calculateBill(10, 20, 5);
+// console.log(calculateBill);
+
+// this how I have been testing without Jest
+// calculateBill(10, 20, 5);
+// calculateBill("barry", 10, 20);
+// calculateBill(100, "barry", 30);
+// calculateBill(100, 40, "barry");
