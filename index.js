da66f368c73f7f8add7915863b3f979e69ee7496
thatresolvestcg <thatresolvestcg@gmail.com>
2019-10-11 14:24:35 +0100

    added first test in Jest

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-import { type } from "os";
-
-// create a function -- this is todo
-// this function will need three arguments
-// we will need to return a string with a currency sign £ so maybe "£" + var
-
 
 const calculateBill = (amount, vat, tip) => {
   if (typeof amount !== "number") {
