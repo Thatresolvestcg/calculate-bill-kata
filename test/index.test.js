ae5f33646d7072173c0809716a5158fb87471209
thatresolvestcg <thatresolvestcg@gmail.com>
2019-10-11 16:30:16 +0100

    need to fix the failing tests

--- test/index.test.js
+++ test/index.test.js
@@ -3,3 +3,16 @@ const calculateBill = require('../index');
 test("calculateBill is a function", () => {
     expect(calculateBill).toBeDefined();
 });
+
+test("it returns a string", () => {
+    const result = calculateBill();
+    expect(typeof calculateBill).toBe('string');
+});
+
+test("it returns the correct amount", () => {
+    const amount = 100;
+    const vat = 20;
+    const tip = 10;
+    expect(calculateBill(amount, vat, tip).toBe("£130"))
+
+});
